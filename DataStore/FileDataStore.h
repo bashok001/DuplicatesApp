4cbdfd0afa88c5d0d6790b10b992ead8044bf035
Ashok Bommisetti <bashok001@gmail.com>
2015-01-29 13:36:26 -0500

    Added Display and Modified the Place of DataStore

--- DataStore/FileDataStore.h
+++ DataStore/FileDataStore.h
@@ -9,17 +9,24 @@
 class FileDataStore {
 	using FilePath = std::string;
 	using FileName = std::string;
+	using FullyQualifiedFileName = std::string;
+	using FQPN = FullyQualifiedFileName;
 	using FilePathSet = std::set < FilePath > ;
 	using FilePathSetIter = FilePathSet::iterator;
 	using FilePathSetIterList = std::list<FilePathSetIter>;
 	using FileCatalog = std::map < FileName, FilePathSetIterList >;
 	using FileCatalogIter = FileCatalog::iterator;
+	using ResultList = std::list < FQPN >;
 	
 	public:
-
+		ResultList get(FileName);
+		
+		FileCatalogIter begin();
+		FileCatalogIter end();
 	private:
 		FileCatalog fileCatalog;
 		FilePathSet filePaths;
+
 
 };
 #endif
