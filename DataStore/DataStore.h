6818558a3f005574a50c6b2aae81504ea44f3008
Ashok Bommisetti <bashok001@gmail.com>
2015-01-29 11:30:02 -0500

    Declared the Structure and Added primary files

---
+++ DataStore/DataStore.h
@@ -0,0 +1,14 @@
+#ifndef DATASTORE_H
+#define DATASTORE_H
+
+#include <string>
+
+class DataStore {
+	using filePath = std::string;
+	using fileName = std::string;
+
+	public:
+
+	private:
+};
+#endif
