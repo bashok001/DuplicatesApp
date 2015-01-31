48b8d30e350c2dd88417d6056eba5dd8e3c69013
Ashok Bommisetti <bashok001@gmail.com>
2015-01-31 16:39:25 -0500

    File Data Store API complete

--- DataStore/FileDataStore.cpp
+++ DataStore/FileDataStore.cpp
@@ -1,42 +1,37 @@
 #include "FileDataStore.h"
 #include <iostream>
 
-
-//FileDataStore::ResultList FileDataStore::get(const FileDataStore::FileName& fileName) {
-//
-//}
-
-//void FileDataStore::put(const FileDataStore::FQPN& fqpn) {
-//	FileDataStore::FileName fileName;
-//	FileDataStore::FilePath filePath;
-//	FileDataStore::fqpnSplitter(fqpn, fileName, filePath);
-//	FileDataStore::put(filePath, fileName);
-//}
+FileDataStore::ResultList FileDataStore::get(const FileDataStore::FileName& fileName) {
+	FileDataStore::ResultList resultList;
+	std::string fqpn;
+	if (FileDataStore::fileCatalog.count(fileName) > 0) {
+		FileDataStore::FilePathSetIterList filePathSetIterList = FileDataStore::fileCatalog[fileName];
+		FileDataStore::FilePathSetIter filePathSetIter;
+		for (auto filePathSetIter : filePathSetIterList) {
+			fqpn = (*filePathSetIter).c_str() + std::string("\\") + fileName;
+			resultList.push_back(fqpn);
+		}
+	}
+	return resultList;
+}
 
 void FileDataStore::put(const FileDataStore::FilePath& filePath, const FileDataStore::FileName& fileName) {
 	FileDataStore::FilePathSetInsertReturn fpSetReturn = FileDataStore::filePaths.insert(filePath);
-
 	if (FileDataStore::fileCatalog.count(fileName) > 0) {
-		if (fpSetReturn.second == false) {
-			std::cout << "Repeated File Encountered in Insertion Process \n";
-			std::cout << FileDataStore::fileCatalog.size() << "\n";
+		FileDataStore::FilePathSetIterList& filePathSetIterList = FileDataStore::fileCatalog[fileName];
+		if (fpSetReturn.second == false && std::find(filePathSetIterList.begin(), filePathSetIterList.end(), fpSetReturn.first) != filePathSetIterList.end()) {
+			//TODO: EXCEPTION
+			std::cout << "***********EXCEPTION************* \n";
 		}
 		else {
-			std::cout << "Condition Two: FileName Found PathName Didnt\n";
-			//FileDataStore::FileCatalogIter fileCatalogEntry = FileDataStore::fileCatalog.find(filePath);
-			FileDataStore::FilePathSetIterList asd = FileDataStore::fileCatalog[filePath];
-			asd.push_back(fpSetReturn.first);
-			std::cout << FileDataStore::fileCatalog.size() << "\n";
-			//fileCatalogEntry->second;
-			/*push_back(fpSetReturn.first); */
+			FileDataStore::FilePathSetIterList& filePathSetIterList = FileDataStore::fileCatalog[fileName];
+			(filePathSetIterList).push_back(fpSetReturn.first);
 		}
 	}
 	else {
-		std::cout << "Condition Three: New File\n";
 		FileDataStore::FilePathSetIterList newFilePathsList;
 		newFilePathsList.push_back(fpSetReturn.first);
 		FileDataStore::fileCatalog.insert(std::pair<FileName, FilePathSetIterList>(fileName, newFilePathsList));
-		std::cout << FileDataStore::fileCatalog.size() << "\n";
 	}
 
 }
@@ -61,44 +56,84 @@ FileDataStore::FileCatalogIter FileDataStore::end() {
 FileDataStore::FilePathSetIter FileDataStore::getFilePathSetIter(const FileDataStore::FilePath& filePath) {
 	return FileDataStore::filePaths.find(filePath);
 }
+#ifdef TEST_ENV
+void FileDataStore::dump() {
+	std::cout << "Dumping data from catalog & List \n";
+	for (auto fileCatalogItem : FileDataStore::fileCatalog) {
+		std::cout << fileCatalogItem.first << "\t";
+		FileDataStore::FilePathSetIterList filePathSetIterList = FileDataStore::fileCatalog[fileCatalogItem.first];
 
-//DELETE THIS
-void FileDataStore::fullSizeDet() {
-	std::cout << "Catalog Size is: " <<FileDataStore::fileCatalog.size() << "\n";
-	std::cout << "PathSet Size is: " << FileDataStore::filePaths.size() << "\n";
+		for (auto filePathList : filePathSetIterList)
+			std::cout << (*filePathList).c_str() << "\n" << "\t";
+		std::cout << "\n";
+	}
+
+	std::cout << "\n" << "Dumping data from FilePathSet" << "\n";
+	for (auto path : FileDataStore::filePaths)
+		std::cout << path << "\n";
 }
+#endif
 
 #ifdef TEST_DATASTORE
 #define SUCCESS 1
 #define FAIL 0
 int main() {
 	int TEST_SUCCESS = 1;
-	
+
 	std::cout << "Testing DataStore Initiated \n";
 	FileDataStore fds;
-	
+
 	if (fds.size() == 0) {
 		TEST_SUCCESS = TEST_SUCCESS*SUCCESS;
 	}
 	else {
 		TEST_SUCCESS = TEST_SUCCESS*FAIL;
 	}
-	
-	fds.put("C", "ASHOK");
-	fds.put("C", "ASHOK2");
-	fds.put("C", "ASHOK3"); 
-	fds.put("C", "ASHOK4"); 
-	fds.put("C", "ASHOK4");
-	fds.put("C", "ASHOK5");
-	fds.put("D", "ASHOK5");
 
-	fds.fullSizeDet();
-	if (fds.size() == 5){
-		
+	fds.put("C:", "ASHOK");
+	fds.put("C:", "ASHOK");
+	fds.put("E:", "ASHOK");
+
+	fds.put("D:", "ASHOK4");
+	fds.put("E:", "ASHOK4");
+
+	fds.put("C:", "ASHOK2");
+
+	fds.put("C:", "ASHOK3");
+
+	fds.put("C:", "ASHOK5");
+	fds.put("D:", "ASHOK5");
+	fds.put("E:", "ASHOK5");
+	fds.put("F:", "ASHOK5");
+	fds.put("G:", "ASHOK5");
+
+	if (fds.size() == 5) {
 		TEST_SUCCESS = TEST_SUCCESS*SUCCESS;
 	}
+	else {
+		TEST_SUCCESS = TEST_SUCCESS*FAIL;
+	}
 
-	
+	std::list < std::string > list = fds.get("ASHOK5");
+	if (list.size() == 5) {
+		TEST_SUCCESS = TEST_SUCCESS*SUCCESS;
+	}
+	else {
+		TEST_SUCCESS = TEST_SUCCESS*FAIL;
+	}
+
+	fds.dump();
+
+	fds.flush();
+
+	if (fds.size() == 0) {
+		TEST_SUCCESS = TEST_SUCCESS*SUCCESS;
+	}
+	else {
+		TEST_SUCCESS = TEST_SUCCESS*FAIL;
+	}
+
+
 	std::cout << ((TEST_SUCCESS) ? "All Tests Passed\n" : "Something Failed\n");
 	return TEST_SUCCESS;
 }
--- DataStore/FileDataStore.h
+++ DataStore/FileDataStore.h
@@ -13,30 +13,29 @@ class FileDataStore {
 	using FQPN = FullyQualifiedFileName;
 	using FilePathSet = std::set < FilePath > ;
 	using FilePathSetIter = FilePathSet::iterator;
-	using FilePathSetIterList = std::list<FilePathSetIter>;
-	using FileCatalog = std::map < FileName, FilePathSetIterList >;
+	using FilePathSetIterList = std::list < FilePathSetIter > ;
+	using FileCatalog = std::map < FileName, FilePathSetIterList > ;
 	using FileCatalogIter = FileCatalog::iterator;
-	using ResultList = std::list < FQPN >;
-	using FilePathSetInsertReturn = std::pair<FilePathSetIter, bool>;
-	using FileCatalogInsertReturn = std::pair <FileCatalogIter , bool> ;
+	using ResultList = std::list < FQPN > ;
+	using FilePathSetInsertReturn = std::pair < FilePathSetIter, bool > ;
+	using FileCatalogInsertReturn = std::pair < FileCatalogIter, bool > ;
 
-	public:
-		ResultList get(const FileName& fileName);
-		void put(const FQPN& fqpn);
-		void put(const FilePath& filePath, const FileName& fileName);
-		int size();
-		void flush();
-		
-		FileCatalogIter begin();
-		FileCatalogIter end();
-	
-		//DELETE THIS API CALL
-		void fullSizeDet();
-	private:
-		FileCatalog fileCatalog;
-		FilePathSet filePaths;
-		FilePathSetIter getFilePathSetIter(const FilePath& filePath);
-		void fqpnSplitter(const FQPN& fqpn, const FilePath& filePath, const FileName& fileName);
+public:
+	ResultList get(const FileName& fileName);
+	void put(const FilePath& filePath, const FileName& fileName);
+	int size();
+	void flush();
+#ifdef TEST_ENV
+	void dump();
+#endif
+	FileCatalogIter begin();
+	FileCatalogIter end();
+
+private:
+	FileCatalog fileCatalog;
+	FilePathSet filePaths;
+	FilePathSetIter getFilePathSetIter(const FilePath& filePath);
+
 
 };
 #endif
